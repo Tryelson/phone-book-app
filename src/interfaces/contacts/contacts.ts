58f71d7a2fa419639aaef534eaedfdd3a12fee2a
Tryelson Marques <tryelson_henrique@hotmail.com>
2023-12-17 23:31:10 -0300

    feat: added new package and new interfaces

--- src/interfaces/contacts/contacts.ts
+++ src/interfaces/contacts/contacts.ts
@@ -2,5 +2,11 @@ export interface Contact{
     id: string,
     firstName: string,
     lastName?: string,
+    fullName: string,
     phoneNumber: string
 }[] || undefined
+
+export interface FetchDataResponse {
+    success: boolean,
+    contacts: Contact[];
+  }
